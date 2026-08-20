b6552d3666ea167de32162dd28cd92757813ff5f
Joseph Christian Mutia <josephchristianmutia@gmail.com>
2026-08-20 13:06:43 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,12 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        length = len(cleaned)
+        print(length)
+
+        for i in range(length):
+            if cleaned[i] != cleaned[length - i - 1]:
+                return False
+        return True
